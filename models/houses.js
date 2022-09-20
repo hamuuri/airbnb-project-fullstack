39b5547c7f5d2af5ea4339a7001f27fe0f8e56c7
hamuuri <mohammed_125@outlook.com>
2022-09-20 15:05:20 +0700

    created the modules

---
+++ models/houses.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+module.exports = mongoose.model('houses', {
+  description: {
+    type: String,
+    required: true
+  },
+  host: {
+    type: _id,
+    required: true,
+    ref: 'users'
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  photos: [
+    {
+      type: String
+    }
+  ],
+  price: {
+    type: Number,
+    required: true
+  },
+  rooms: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+})
---
+++ models/reviews.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+module.exports = mongoose.model('reviews', {
+  author: {
+    type: _id,
+    required: true,
+    ref: 'users'
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  house: {
+    type: _id,
+    required: true,
+    ref: 'houses'
+  }
+})
